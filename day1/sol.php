1d5f5bd6c8739d2cdd7a61f1fbd6ca30cb7bc2cf
Earncef Sequeira <earncef@earncef.com>
2015-12-03 17:43:25 +0530

    Cleanup

--- day1/sol.php
+++ day1/sol.php
@@ -1,6 +1,6 @@
 <?php
-	$input = file_get_contents('input');
-	$compute = create_function('', 'return ' . str_replace(['(', ')'], ['+1', '-1'], $input) . ';');
-	for($position = 1; $input[0] != ')' && $p = strpos($input, '()') + 1; $position += 2)
-		$input = substr_replace($input, '', $p - 1, 2);
-	echo "{$compute()} {$position}";
+$input = file_get_contents('input');
+$compute = create_function('', 'return ' . str_replace(['(', ')'], ['+1', '-1'], $input) . ';');
+for($position = 1; $input[0] != ')' && $p = strpos($input, '()') + 1; $position += 2)
+    $input = substr_replace($input, '', $p - 1, 2);
+echo "{$compute()} {$position}";
--- day3/sol.php
+++ day3/sol.php
@@ -1,18 +1,17 @@
 <?php
-	$input = file_get_contents('input');
-	$length = strlen($input);
-	$setPosition = function(&$x, &$y, &$houses, $c) {
-		$x = ($c == '>') ? ++$x : (($c == '<') ? --$x : $x);
-		$y = ($c == '^') ? ++$y : (($c == 'v') ? --$y : $y);
-		$houses["{$x}x{$y}"] = 1;
-	};
-	for ($i = $x = $y = $rx = $ry = $sx = $sy = 0, $houses = $rHouses = $sHouses = ['0x0' => 1]; $i < $length && $c = $input[$i]; $i++) {
-		$setPosition($x, $y, $houses, $c);
-		if ($i % 2 == 0) {
-			$setPosition($sx, $sy, $sHouses, $c);
-		} else {
-			$setPosition($rx, $ry, $rHouses, $c);
-		}
-	}	
-	echo count($houses), ' ', count(array_merge($sHouses, $rHouses));
-	
+$input = file_get_contents('input');
+$length = strlen($input);
+$setPosition = function(&$x, &$y, &$houses, $c) {
+    $x = ($c == '>') ? ++$x : (($c == '<') ? --$x : $x);
+    $y = ($c == '^') ? ++$y : (($c == 'v') ? --$y : $y);
+    $houses["{$x}x{$y}"] = 1;
+};
+for ($i = $x = $y = $rx = $ry = $sx = $sy = 0, $houses = $rHouses = $sHouses = ['0x0' => 1]; $i < $length && $c = $input[$i]; $i++) {
+    $setPosition($x, $y, $houses, $c);
+    if ($i % 2 == 0) {
+        $setPosition($sx, $sy, $sHouses, $c);
+    } else {
+        $setPosition($rx, $ry, $rHouses, $c);
+    }
+}
+echo count($houses), ' ', count(array_merge($sHouses, $rHouses));
