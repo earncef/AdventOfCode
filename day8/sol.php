be734dbe3faab0dbf7c45ee66fcffd2afd155e37
Earncef Sequeira <earncef@earncef.com>
2015-12-08 12:34:13 +0530

    Cleanup

--- day8/sol.php
+++ day8/sol.php
@@ -1,6 +1,7 @@
 <?php
 $input = file_get_contents('input');
+$rawLength = strlen(str_replace("\n", '', $input));
 $compute = create_function('', 'return ' . str_replace("\n",'.',$input) . ';');
-$total = strlen(str_replace("\n", '', $input)) - strlen($compute());
-$total2 = strlen(str_replace("\n", '""', addslashes($input))) + 2 - strlen(str_replace("\n", '', $input));
+$total = $rawLength - strlen($compute());
+$total2 = strlen(str_replace("\n", '""', addslashes($input))) + 2 - $rawLength;
 echo "$total $total2";
