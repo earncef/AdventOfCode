5e45854155f753cb8a02969cf3fc316baff1f927
Earncef Sequeira <earncef@earncef.com>
2015-12-10 06:47:16 +0530

    Rewrote solution as a function

--- day4/sol.php
+++ day4/sol.php
@@ -1,7 +1,8 @@
 <?php
 $input = file_get_contents('input');
-for ($i = 1, $md5 = ''; substr($md5, 0, 5) !== '00000'; $i++)
-    $md5 = md5($input . $i);
-for ($j = 1, $md5 = ''; substr($md5, 0, 6) !== '000000'; $j++)
-    $md5 = md5($input . $j);
-echo $i-1, ' ', $j-1;
+function process($input, $prefix) {
+    for ($i = 1, $md5 = '', $l = strlen($prefix); substr($md5, 0, $l) !== $prefix; $i++)
+        $md5 = md5($input . $i);
+    return --$i;
+}
+echo process($input, '00000'), ' ', process($input, '000000');
