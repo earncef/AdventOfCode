88198729ca45eed4b41f60c576d14a3a4b26c4dd
Earncef Sequeira <earncef@earncef.com>
2015-12-11 14:34:36 +0530

    cleanup

--- day2/sol.php
+++ day2/sol.php
@@ -1,9 +1,9 @@
 <?php
 $input = explode("\n", trim(file_get_contents('input')));
 $count = count($input);
-for ($totalLength = $totalArea = $i = 0; $i < $count && $dim = explode('x', $input[$i]); $i++) {
+for ($length = $area = $i = 0; $i < $count && $dim = explode('x', $input[$i]); $i++) {
     sort($dim);
-    $totalArea += ($dim[0] * $dim[1] + $dim[1] * $dim[2] + $dim[0] * $dim[2]) * 2 + $dim[0] * $dim[1];
-    $totalLength += ($dim[0] + $dim[1]) * 2 + ($dim[0] * $dim[1] * $dim[2]);
+    $area += ($dim[0] * $dim[1] + $dim[1] * $dim[2] + $dim[0] * $dim[2]) * 2 + $dim[0] * $dim[1];
+    $length += ($dim[0] + $dim[1]) * 2 + ($dim[0] * $dim[1] * $dim[2]);
 }
-echo "{$totalArea} {$totalLength}";
+echo "{$area} {$length}";
