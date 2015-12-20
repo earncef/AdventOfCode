0c9944b2bada1ad0dc2f672a35fa51f74317a2c0
Earncef Sequeira <earncef@earncef.com>
2015-12-20 11:25:44 +0530

    Cleanup

--- day20/sol.php
+++ day20/sol.php
@@ -1,22 +1,17 @@
 <?php
 $input = file_get_contents('input');
+echo findHouse($input), ' ', findHouse($input, 50);
 
 function findHouse($required, $maxHouses = null) {
-    $presents = $house = 0;
-    while ($presents < $required) {
-        $house++;
+    for ($presents = $house = 0; $presents < $required; $house++)
         $presents = numPresents($house, $maxHouses);
-    }
-    return $house;
+    return --$house;
 }
 
 function numPresents($house, $maxHouses = null) {
-    for ($i = 2, $ret = 1, $c = sqrt($house); $i <= $c && ! ($maxHouses !== null && --$maxHouses < 1); $i++) {
+    for ($i = 2, $ret = 1, $c = sqrt($house); $i <= $c && ! ($maxHouses !== null && --$maxHouses < 1); $i++)
         if ($house % $i == 0) if (($ret += $i) && $i != $house / $i) $ret += $house / $i;                    
-    }
-    if ($maxHouses !== null) return ($ret + $house) * 11;
-    return ($ret + $house) * 10;
+    return ($maxHouses !== null) ? ($ret + $house) * 11 : ($ret + $house) * 10;
 }
 
 
-echo findHouse($input), ' ', findHouse($input, 50);
